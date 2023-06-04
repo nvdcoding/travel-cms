94a5b9cb35cef8d09df14c20a23ce185a01fa7c9
lamlammai <dangthimailam25@gmail.com>
2023-06-05 02:31:01 +0700

    upcode0506

--- src/page/QL-tour/index.js
+++ src/page/QL-tour/index.js
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 import "../../assets/css/home.css";
 import { Tabs } from "antd";
 import Layout from "../../components/layout/layout";
-import PheDuyet from "./pheduyet";
 import YeuCau from "./yeucau";
 import Tour from "./tour";
+import TuChoi from "./tuchoi";
 
 export default function ManageTour() {
   useEffect(() => {}, []);
@@ -16,11 +16,11 @@ export default function ManageTour() {
             <Tabs.TabPane tab="Yêu cầu phê duyệt" key="1">
               <YeuCau />
             </Tabs.TabPane>
-            {/* <Tabs.TabPane tab="Lịch sử phê duyệt" key="2">
-              <PheDuyet />
-            </Tabs.TabPane> */}
             <Tabs.TabPane tab="Danh sách tour" key="3">
               <Tour />
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Tour từ chối" key="2">
+              <TuChoi />
             </Tabs.TabPane>
           </Tabs>
           ;
---
+++ src/page/QL-tour/tuchoi.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import "../../assets/css/home.css";
+import { Table, message } from "antd";
+import { sendGet } from "../../utils/api";
+
+export default function TuChoi() {
+  const [data, setData] = useState([]);
+  const columns = [
+    {
+      title: "STT",
+      dataIndex: "stt",
+      width: "40px",
+      render: (_, record, index) => <>{index + 1}</>,
+    },
+    {
+      title: "Thời gian từ duyệt",
+      dataIndex: "time",
+      render: (_, record) => (
+        <>{new Date(record?.createdAt).toLocaleString()}</>
+      ),
+    },
+    {
+      title: "Tên tour",
+      dataIndex: "name",
+    },
+    {
+      title: "Tên HDV",
+      dataIndex: "nameGuide",
+      render: (_, record) => <>{record.tourGuide?.name}</>,
+    },
+    {
+      title: "Tỉnh thành",
+      dataIndex: "provice",
+      render: (_, record) => <>{record.province?.name}</>,
+    },
+    {
+      title: "Trạng thái",
+      dataIndex: "status",
+      render: (_, record) => (
+        <>{record.status == 1 ? "Đang hoạt động" : "Bị khóa"}</>
+      ),
+    },
+  ];
+  const [tableParams, setTableParams] = useState({
+    pagination: {
+      current: 1,
+      pageSize: 8,
+      total: data.length,
+    },
+  });
+  const handleTableChange = (pagination, filters, sorter) => {
+    setTableParams({
+      pagination,
+      filters,
+      ...sorter,
+    });
+  };
+  const ListTour = async () => {
+    const result = await sendGet("/tours/admin/rejected-list", { limit: 100 });
+    if (result.returnValue.data.length >= 0) {
+      setData(result.returnValue.data);
+    } else {
+      message.error("thất bại");
+    }
+  };
+  useEffect(() => {
+    ListTour();
+  }, []);
+  return (
+    <>
+      <Table
+        rowKey={(record) => record.id}
+        className="table-custom-user"
+        columns={columns}
+        dataSource={data}
+        onChange={handleTableChange}
+        pagination={tableParams.pagination}
+      />
+    </>
+  );
+}
